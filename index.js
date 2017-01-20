cf0416d64febd9e8db5f6ddf0f320e26d55f5879
David Weber <davidjwe33@gmail.com>
2017-01-20 12:40:26 -0800

    installed initial npm packages, set up middleware, and created Mongodb

---
+++ index.js
@@ -0,0 +1,60 @@
+var
+    express = require('express'),
+    app = express()
+    ejs = require('ejs'),
+    ejsLayouts = require('express-ejs-layouts'),
+    mongoose = require('mongoose'),
+    flash = require('connect-flash'),
+    logger = require('morgan'),
+    bodyParser = require('body-parser'),
+    cookieParser = require('cookie-parser'),
+    session = require('express-session'),
+    MongoStore = require('connect-mongo')(session),
+    passport = require('passport'),
+    methodOverride = require('method-override'),
+    dotenv = require('dotenv').load({silent: true}),
+    http = require('http').Server(app)
+
+
+    ///mongoose
+    var port = process.env.PORT || 3000
+    var mongoConnectionString = process.env.MONGO_URL
+
+    mongoose.connect(process.env.MONGO_URL, function(err){
+      	if(err) return console.log('Cannot connect to Mongo')
+      	console.log('Connected to MongoDB. Hell Yeah!')
+    })
+
+
+    app.use(logger('dev'))
+    app.use(methodOverride('_method'))
+    app.use(cookieParser())
+    app.use(bodyParser.urlencoded({extended: true}))
+    app.use(bodyParser.json())
+    app.use(session({
+    	secret: 'petersplan',
+    	cookie: {maxAge: 6000000},
+    	resave: true,
+    	saveUninitialized: false,
+      store: new MongoStore({url: mongoConnectionString})
+    }))
+    app.use(passport.initialize())
+    app.use(passport.session())
+    app.use(flash())//flash application
+
+    //this will add a currentUser to be available in every view
+    app.use(function(req,res,next){
+      if(req.user) req.app.locals.currentUser = req.user
+      req.app.locals.loggedIn = !!req.user
+      next()
+    })
+
+    // ejs configuration
+    app.set('view engine', 'ejs')
+    app.use(ejsLayouts)
+    app.use(express.static('public'))
+
+
+http.listen(port, function(){
+	console.log("Server running on port: ", port)
+})
